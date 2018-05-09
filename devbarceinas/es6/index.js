cac5d63df917dfd6e415732e12395dee90cc0f46
Mario Joaquin Lopez Barceinas <mario.lopez@stkbancomerans.com>
2018-05-08 23:25:44 -0500

    He modificado en especial el archivo datos.json - (El cambio fue renombrar las variables de español a ingles)

--- devbarceinas/es6/index.js
+++ devbarceinas/es6/index.js
@@ -2,20 +2,20 @@
 (function() {
     'use strict';
 
-    const personList = document.querySelector('.person-list');
+    const personList = document.getElementById('person-list');
     const API_URL = 'datos.json';
 
     fetch(API_URL)
         .then(response => response.json())
-        .then(response => printResponse(response.datos))
+        .then(response => printResponse(response.data))
         .catch(err => console.log(`Fallaste en la petición ${err}`))
 
     function printResponse(datos) {
         for (let position in datos) {
-            let value = datos[position].nombre
+            let value = datos[position].name;
             /**
-             * Object.keys devuelve un array cuyos elementos 
-             * son strings correspondientes a las propiedades 
+             * Object.keys devuelve un array cuyos elementos
+             * son strings correspondientes a las propiedades
              * enumerables que seencuentran directamente en el object.
              */
             personList.innerHTML += `
